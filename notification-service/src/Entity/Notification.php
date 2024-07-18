b392fd6018e998f57913da4d02775be23d3a9e79
Evan STEVANT <stevantevan04@gmail.com>
2024-07-18 12:04:58 +0200

    a

--- notification-service/src/Entity/Notification.php
+++ notification-service/src/Entity/Notification.php
@@ -1,5 +1,7 @@
 <?php
 
+// src/Entity/Notification.php
+
 namespace App\Entity;
 
 use App\Repository\NotificationRepository;
@@ -10,55 +12,59 @@ class Notification
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'integer')]
+    private $id;
 
-    #[ORM\Column(length: 255)]
-    private ?string $email_recipient = null;
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email_recipient;
 
-    #[ORM\Column(length: 255)]
-    private ?string $message = null;
+    #[ORM\Column(type: 'string', length: 255)]
+    private $sujet;
 
-    #[ORM\Column(length: 255)]
-    private ?string $sujet = null;
+    #[ORM\Column(type: 'text')]
+    private $message;
 
+    // Getter and setter for id (if needed)
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    // Getter and setter for email_recipient
     public function getEmailRecipient(): ?string
     {
         return $this->email_recipient;
     }
 
-    public function setEmailRecipient(string $email_recipient): static
+    public function setEmailRecipient(string $email_recipient): self
     {
         $this->email_recipient = $email_recipient;
 
         return $this;
     }
 
-    public function getMessage(): ?string
-    {
-        return $this->message;
-    }
-
-    public function setMessage(string $message): static
-    {
-        $this->message = $message;
-
-        return $this;
-    }
-
+    // Getter and setter for sujet
     public function getSujet(): ?string
     {
         return $this->sujet;
     }
 
-    public function setSujet(string $sujet): static
+    public function setSujet(string $sujet): self
     {
         $this->sujet = $sujet;
+
+        return $this;
+    }
+
+    // Getter and setter for message
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
 
         return $this;
     }
